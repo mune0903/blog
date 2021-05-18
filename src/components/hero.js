17df1fc101e9eadb85fe97c8399c4f762e321556
mune0903 <b1016051@fun.ac.jp>
2021-05-18 23:13:59 +0900

    Initial commit

---
+++ src/components/hero.js
@@ -0,0 +1,20 @@
+import React from "react"
+import "../styles/hero.css"
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <h1 className="hero-text">
+                Welcome to my blog!<br />
+                Enjoy lots of posts.<br />
+            </h1>
+            <div className="hero-author">
+                <img src="https://avatars.githubusercontent.com/u/28261278?v=4" className="hero-author-image" alt="avatar"></img>
+                <p className="hero-author-text">
+                    Written by XXXX.<br />
+                    Front Engineer at Hoge.Inc. Love JavaScript.
+                </p>
+            </div>
+        </div>
+    )
+}
